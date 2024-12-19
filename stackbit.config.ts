1e99347c2ab1d95c93cfb7209e012d255d7f8d60
Nathan Bycroft <97001906+nathanbycroft@users.noreply.github.com>
2024-12-19 16:52:31 +1300

    Update stackbit.config.ts
    devCommand

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,6 +2,7 @@ import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
+    "devCommand": "npm run dev",
     "nodeVersion": "18",
     "ssgName": "custom",
     "contentSources": [],
